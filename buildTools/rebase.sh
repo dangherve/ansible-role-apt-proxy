912cf86720803269f8a343cf47e5410ee42f4209
dangherve <dangherve@gmail.com>
2026-03-12 13:45:30 -0400

    autorebase tools

---
+++ buildTools/rebase.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+set -e
+git remote add upstream https://github.com/sitedyno/ansible-role-apt-proxy.git
+git fetch upstream
+git rebase upstream/master
+git remote set-url origin git@github.com:dangherve/ansible-role-apt-proxy.git
+git push --force --set-upstream origin master
